67f1c550ad56c1afeac7f41eca4e57c507c89c7f
Thamjid Thachu <91719380+thamjidthachu@users.noreply.github.com>
2022-05-23 15:15:25 +0530

    add Static Folder

--- flask_crud-db.py
+++ flask_crud-db.py
@@ -3,5 +3,5 @@ import sqlite3
 con = sqlite3.connect('studentData.db')
 print ("Database Created")
 
-con.execute('create table student(studid integer primary key autoincrement,name text,age number,email text,place text,username text,password text,status text default "No" NOT NULL,teacher text default "No" NOT NULL,admin text default "No" NOT NULL)')
+# con.execute('create table student(studid integer primary key autoincrement,name text,age number,email text,place text,username text,password text,status text default "no" NOT NUL,teacher text default "No" NOT NUL,admin text default "No" NOT NULL')
 print("Table Created")
